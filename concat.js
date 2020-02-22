63a7d5cd1b42645092fd4b8ecaff236065fadf8e
asosunoff <al.sosunoff@gmail.com>
2020-02-22 17:53:09 +0300

    concat

---
+++ concat.js
@@ -0,0 +1,28 @@
+const async = require('async');
+
+async.concat([test_1, test_2, test_3], (item, callback) => {
+    item(callback);
+}, (err, result) => {
+    if(err)
+        console.log('Error', err);
+
+    console.log(result);
+});
+
+function test_1(callback){
+    setTimeout(() => {
+        callback(null, 1);
+    }, 1000);
+}
+
+function test_2(callback){
+    setTimeout(() => {
+        callback(null, 2);
+    }, 1000);
+}
+
+function test_3(callback){
+    setTimeout(() => {
+        callback(null, 3);
+    }, 1000);
+}
